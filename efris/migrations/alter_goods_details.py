a365a74af9448e4b4829f8bd17233d229ad0f80f
Ernest <ernestben69@gmail.com>
2024-09-10 14:41:02 +0300

    Fixed some bugs

--- efris/migrations/alter_goods_details.py
+++ efris/migrations/alter_goods_details.py
@@ -1,12 +1,23 @@
 import frappe
 
 def alter_goods_details_schema():
-    # Alter the 'name' column to VARCHAR(500)
     try:
+        # Alter the 'name' column in the 'Goods Details' table
         frappe.db.sql("""
             ALTER TABLE `tabGoods Details` 
             MODIFY COLUMN `name` VARCHAR(500);
         """)
-        frappe.db.commit()  # Commit the changes
+        
+        # Alter the 'deleted_name' column in the 'Deleted Document' table
+        frappe.db.sql("""
+            ALTER TABLE `tabDeleted Document` 
+            MODIFY COLUMN `deleted_name` VARCHAR(500);
+        """)
+        
+        # Commit the changes
+        frappe.db.commit()
+        
     except Exception as e:
+        # Log any errors encountered during the schema alteration
         frappe.log_error(message=str(e), title="Schema Alteration Error")
+
