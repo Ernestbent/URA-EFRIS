d47526c2994350c50bc4b14d3a09ac5dbb95b960
Ernest <ernestben69@gmail.com>
2024-10-21 10:43:32 +0300

    Pushing to Production

--- efris/hooks.py
+++ efris/hooks.py
@@ -30,12 +30,12 @@ app_include_js = "/assets/efris/js/integration_request_widget.js"
 
 # include js in doctype views
 doctype_js = {
-    "Item":"public/js/fetch_efris_items.js",
-    "Item":"public/js/toggle_excise_duty.js",
+    "Item":"public/js/fetch_efris_items.js", 
     "Purchase Invoice":"public/js/filter_items_pi.js",
     "Sales Invoice":"public/js/filter_items_si.js",
     "Item":"public/js/toggle_excise_duty.js",
     "Stock Entry":"public/js/filter_items_stock.js",
+    "Item":"public/js/toggle_excise_duty.js",
     "Sales Invoice":"public/js/credit_reason.js",
     "Item": "public/js/item_custom_button.js",  # JavaScript file for the Item doctype
     "Sales Order": "public/js/sales_order_custom_button.js",  # Example for another doctype
